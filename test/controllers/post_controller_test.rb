fdc0bc71884258d660fa3e98bedc05c6c3e365db
Rei Kojima <juv0725rei@gmail.com>
2019-02-12 15:51:24 +0900

    Add a full_title helper

--- test/controllers/post_controller_test.rb
+++ test/controllers/post_controller_test.rb
@@ -4,19 +4,19 @@ class PostControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get post_home_url
     assert_response :success
-    assert_select "title", "Home | Tweet App"
+    assert_select "title", "Tweet App"
   end
 
   test "should get help" do
     get post_help_url
     assert_response :success
-    assert_select "title", "Help | Tweet App"
+    assert_select "title", "Tweet App"
   end
 
   test "should get about" do 
     get post_about_url 
     assert_response :success 
-    assert_select "title", "About | Tweet App"
+    assert_select "title", "Tweet App"
   end
 
 end
