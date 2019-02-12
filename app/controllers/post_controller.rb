68476514309948e19cb3641dc8178d870b7f1785
Rei Kojima <juv0725rei@gmail.com>
2019-02-12 14:08:06 +0900

    finish post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -4,4 +4,7 @@ class PostController < ApplicationController
 
   def help
   end
+  
+  def about 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
  
+  root 'post#home'
   get 'post/home'
   get 'post/help'
+  get 'post/about'
 end
