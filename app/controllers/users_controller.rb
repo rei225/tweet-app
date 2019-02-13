378eb129e74a4c5970ca43cce7ee5529f34a4f49
Rei Kojima <juv0725rei@gmail.com>
2019-02-13 19:17:27 +0900

    implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save 
+        log_in @user
         flash[:success] = "welcome to tweet-app"
         redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'post#home'
   get "/help" , to: "post#help"
   get "/about", to: "post#about"
   get "/contact", to:"post#contact"
   get "/signup", to:"users#new"
+  get "/login", to:"sessions#new"
+  post "/login", to:"sessions#create"
+  delete "/logout", to:"sessions#destroy"
   resources :users
 end
