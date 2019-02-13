e53dbd5e7e367947e3934f1dc7b243e9c6c02edb
Rei Kojima <juv0725rei@gmail.com>
2019-02-13 16:08:40 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
-  def new
+  def show 
+    @user = User.find_by(params[:id])
   end
+  def new
+    @user = User.new
+  end
+  def create 
+    @user = User.new(user_params)
+    if @user.save 
+        flash[:success] = "welcome to tweet-app"
+        redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private 
+    def user_params 
+      params.require(:user).permit(:name, :email, :password,
+                                    :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
- 
-  get 'users/new'
   root 'post#home'
   get "/help" , to: "post#help"
   get "/about", to: "post#about"
   get "/contact", to:"post#contact"
   get "/signup", to:"users#new"
+  resources :users
 end
